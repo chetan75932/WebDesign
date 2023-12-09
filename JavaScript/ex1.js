e21273390984f32096bb52dd3dec27f80f63c420
chetan75932 <128772786+chetan75932@users.noreply.github.com>
2023-12-09 13:30:41 +0530

    Add files via upload

---
+++ JavaScript/ex1.js
@@ -0,0 +1,11 @@
+var x=100;
+{
+    let x="Sourabh";
+    console.log(x);
+}
+console.log(x);
+
+var y=new String("chetan")
+console.log(y,typeof(y),typeof(x))
+var c="s1"
+console.log(c,typeof(c))
---
+++ JavaScript/ex2.js
@@ -0,0 +1,6 @@
+function add( a, b){
+    return a+b;
+}
+
+let s=add(3,4)
+console.log(s)
