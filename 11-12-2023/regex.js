227da3964e43f325ecb65d33660bacb270f851f1
chetan75932 <128772786+chetan75932@users.noreply.github.com>
2023-12-11 10:02:09 +0530

    regEx,minMaxPatternValidation

---
+++ 11-12-2023/regex.js
@@ -0,0 +1,16 @@
+//091-202-68232832
+
+/*let regex=/[0-9]{2,3}-[0-9]{3}-[0-9]{8}$/
+//let regex=/[0-9]{2,3}/+/-/+/[0-9]{3}/+/-/+/[0-9]{8,8}/
+
+//let s1="91-2022-68232832";
+let s2="10-202-862869258689896"
+
+console.log(s2.match(regex));*/
+
+
+let regex1=/[A-Za-z]{10}@gmail.[com in]{2,3}/
+let email="hfdkdksslw@gmail.com";
+let email2="kddkgjffgg12ijdhggkfgt@gmail.in"
+
+console.log(email2.match(regex1))
